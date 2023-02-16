04d902bffa0a6fb51c63a232c6d77b57ae746288
Jose <jlmg.4589@gmail.com>
2023-02-16 19:56:09 +0100

    Ejercicio que devuelve la letra del DNI

---
+++ Actividades_3/DNI.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Este script calcula la letra del DNI, según el número del mismo.
+
+# Creamos la secuencia de letras.
+LetraDNI=(T R W A G M Y F P D X B N J Z S Q V H L C K E)
+
+
+# Definimos la validación de DNI (0=OK, 1=Error)
+validacion(){
+    len=${#1}
+
+    if [ $len -eq 8 ]; then
+        for i in {1..8}
+        do
+            if [[ ${1:i-1:1} != [0-9] ]]; then
+                return 2
+            fi
+        done
+        return 0
+    fi
+    return 1
+}
+
+
+# Informamos al usuario.
+echo "Este script le va a consultar el número del DNI, para poder determinar su letra."
+read -p "Introduzca el número del DNI: " dni
+
+
+# Obtenemos el resto de 23
+resto=$((dni%23))
+# Validamos el DNI y almacenamos el valor de la operación para comparar.
+validacion $dni
+valDNI=$?
+
+
+# Tratamos el valor devuelto por la función de validación e imprimimos según el resultado
+echo ""
+echo "***********RESULTADO***************"
+echo ""
+if [ $valDNI == 0 ]; then
+    echo "La letra correspondiente para el DNI introducido es: ${LetraDNI[$resto]}"
+    echo "DNI: $dni${LetraDNI[resto]}"
+elif [ $valDNI == 1 ]; then
+    echo "DNI incorrecto, el número de caracteres es incorrecto."
+elif [ $valDNI == 2 ]; then
+    echo "DNI incorrecto, el DNI introducido contiene caracteres no numéricos."
+fi
